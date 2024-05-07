bb9ea04b90484ee4198cb571677d273fafd45fa9
Divyang Kotian <kotian.divyang@gmail.com>
2024-05-07 12:58:35 -0400

    added basic coloring func with mouse dragging enabled

--- sketch.js
+++ sketch.js
@@ -16,14 +16,31 @@ let gridValue = gridSlider.value;                                   // Initializ
    
    
    function createGrid(value){
+    let isMouseDown=false;                               // initial value is set to off   
     gridContainer.textContent=``;                       // empty the grid to reset on each func call
-        for (i=0;i<value * value;i++){
+        for (i=0;i<value * value;i++){                  // creating a N*N grid
 
             const tempPixel=document.createElement("div");
             gridContainer.appendChild(tempPixel);
+
             tempPixel.style.backgroundColor = "white";
-            tempPixel.setAttribute("class","tempGrid")
+            tempPixel.setAttribute("class","tempGrid")      //adding a class to manipulate CSS externally
+            
+            tempPixel.addEventListener(`mousedown`, (e) =>{
+                isMouseDown=true;                        // set flag to true, to start tracking
+            });                       
+            
+            gridContainer.addEventListener(`mousemove`,(e) =>{                  // tracking mouse movement over the container to allow for dragging and coloring
+                    if (isMouseDown){                                           // if mouse 1 is held down only then change color
+                    (e).target.style.backgroundColor="red";
+                } 
+                })
+                
+                // event listener is added accross the whole document to avoid glitch where user can leave container which will not capture mouseup even in the container
+                document.addEventListener('mouseup',(e)=>{                 // resetting flag upon release of M1
+                    isMouseDown=false;
+                })
         }
-    } 
+    }
 })
 
